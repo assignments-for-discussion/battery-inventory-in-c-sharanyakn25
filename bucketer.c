b9d8993e421206ecb7f0fc305a5dbf6a7d68ab69
sharanyakn25 <109367090+sharanyakn25@users.noreply.github.com>
2022-12-15 16:57:41 +0530

    Update bucketer.c

--- bucketer.c
+++ bucketer.c
@@ -1,35 +1,48 @@
 #include <stdio.h>
-#include <assert.h>
+//#include <assert.h>
 
-struct CountsByUsage {
+/*struct CountsByUsage {
   int lowCount = 0;
   int mediumCount = 0;
   int highCount = 0;
-};
+};*/
 
-struct CountsByUsage countBatteriesByUsage(const int* cycles, int nBatteries) {
+/*struct CountsByUsage countBatteriesByUsage(const int* cycles, int nBatteries) {
+  int nBatteries = sizeof(chargeCycleCounts[]);
   struct CountsByUsage counts = {0, 0, 0};
   return counts;
-}
+}*/
 
 int testBucketingByNumberOfCycles() {
   const int chargeCycleCounts[] = {100, 300, 500, 600, 900, 1000};
-  const int numberOfBatteries = sizeof(chargeCycleCounts) / sizeof(chargeCycleCounts[0]);
-  printf("Counting batteries by usage cycles...\n");
-  struct CountsByUsage counts = countBatteriesByUsage(chargeCycleCounts, numberOfBatteries);
-  assert(counts.lowCount == 2);
-  assert(counts.mediumCount == 3);
-  assert(counts.highCount == 1);
-  printf("Done counting :)\n");
-  for(int i=0;i<numberOfBatteries;i++)
+  const int no_battery = sizeof(chargeCycleCounts[]);
+  //printf("Counting batteries by usage cycles...\n");
+  //struct CountsByUsage counts = countBatteriesByUsage(chargeCycleCounts, numberOfBatteries);
+  int lowCount = 0;
+  int mediumCount = 0;
+  int highCount = 0;
+  
+  for(int i=0;i <= no_battery;i++)
   {
     if(chargeCycleCounts[i] <= 310)
+    {
       lowCount++;
-    elseif(chargeCycleCounts[i] < 929 || 310 < chargeCycleCounts[i])
+      printf("Low");
+    }
+    elseif(chargeCycleCounts[i] < 929 && chargeCycleCounts[i] >310)
+    {
       mediumCount++;
+      printf("Medium");
+    }
     elseif(chargeCycleCounts[i] >= 930)
+    {
       highCount++;
-}
+      printf("High");
+    }
+    else
+      break;
+  }
+
 
 int main() {
   testBucketingByNumberOfCycles();
